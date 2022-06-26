323334f5ec74d27ece370ab1d27796b1a843ad5f
Oleksandr <oleksandr.molchanov@gmail.com>
2022-06-26 18:50:54 +0300

    SKLearn - classification with KNN algorithm - annotations on the plot

--- sklearn_classification.py
+++ sklearn_classification.py
@@ -31,17 +31,17 @@ def classify_KNN(features, labels, x_pred):
     prediction = model.predict(x_test)
 
     # Evaluating the model
-    print('Accuracy score: %s %', accuracy_score(prediction, y_test))
-    print('Confusion matrix: \n', confusion_matrix(prediction, y_test))
+    print('KNN Accuracy score: %s %', accuracy_score(prediction, y_test))
+    print('KNN Confusion matrix: \n', confusion_matrix(prediction, y_test))
 
     # Making a prediction
     y_pred = model.predict(x_pred)
-    print('Prediction - Features: %s | Prediction Result: %s' % (str(x_pred).replace('\n', ''), y_pred))
+    print('KNN Prediction - Features: %s | Prediction Result: %s' % (str(x_pred).replace('\n', ''), y_pred))
 
     return y_pred
 
 
-x_pred = np.array([[0, 0], [10, 20]])
+x_pred = np.array([[0, 0], [10, 20], [3.5, 7.2]])
 y_pred = classify_KNN(X, y, x_pred)
 
 
@@ -53,9 +53,13 @@ plt.ylabel("Y")
 x_data = np.concatenate((X, x_pred))
 y_data = np.concatenate((y, y_pred))
 
+
 # Dataset visualization
 # Label '0' is marked with black color
 # Label '1' is marked with yellow color
 plt.scatter(x_data[:, 0], x_data[:, 1], c=y_data)
+
+for i, txt in enumerate(x_data):
+    plt.annotate(str(y_data[i]) + ' ' + str(x_data[i]), (x_data[i]))
 
 plt.show()
