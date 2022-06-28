82b9763f425fffe893247c9f42cc647113c3fa0b
Oleksandr <oleksandr.molchanov@gmail.com>
2022-06-28 16:18:15 +0300

    SKLearn - classification with kernel SVM algorithms - Visualization

--- sklearn_classification_svm_kernel.py
+++ sklearn_classification_svm_kernel.py
@@ -44,6 +44,34 @@ def classify(model: SVC, x_pred):
     return y_pred
 
 
+# Visualizng the dataset and the predictions
+def visualize(model, x_pred, y_pred):
+    plt.title('SVM Classification. KERNEL TYPE: ' + str(model.kernel).upper())
+    plt.xlabel("X")
+    plt.ylabel("Y")
+
+    x_data = np.concatenate((X, x_pred))
+    y_data = np.concatenate((y, y_pred))
+
+    # Label '0' is marked with black color
+    # Label '1' is marked with yellow color
+    plt.scatter(x_data[:, 0], x_data[:, 1], c=y_data)
+
+    for i, txt in enumerate(x_data):
+        plt.annotate(str(x_data[i]), (x_data[i]))
+
+    h = 0.2 # step size
+    x_min, x_max = x_data[:, 0].min() - 1, x_data[:, 0].max() + 1
+    y_min, y_max = x_data[:, 1].min() - 1, x_data[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm)
+    plt.show()
+
+
 x_pred = np.array([
     [0, 0],
     [10, 20],
@@ -56,12 +84,15 @@ x_pred = np.array([
 
 # Classification with Polynomial Kernel
 model = SVC(kernel='poly', degree=8)
-classify(model, x_pred)
+y_pred = classify(model, x_pred)
+visualize(model, x_pred, y_pred)
 
 # Classification with Gaussian Kernel
 model = SVC(kernel='rbf')
 classify(model, x_pred)
+visualize(model, x_pred, y_pred)
 
 # Classification with Sigmoid Kernel
 model = SVC(kernel='sigmoid')
 classify(model, x_pred)
+visualize(model, x_pred, y_pred)
