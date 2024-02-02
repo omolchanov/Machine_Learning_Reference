239de76a9beeb993bd5c8050853326d8cb6d3666
Alex <oleksandr.molchanov@gmail.com>
2024-02-02 14:58:27 +0200

    Sktime mutlivariative forecast

--- time_series/sktime_basic.py
+++ time_series/sktime_basic.py
@@ -1,9 +1,16 @@
+import warnings
+
 import numpy as np
 import pandas as pd
 
 from sktime.datasets import load_airline
+from sktime.datasets import load_longley
+
 from sktime.utils.plotting import plot_series
+
 from sktime.forecasting.naive import NaiveForecaster
+from sktime.forecasting.var import VAR
+from sktime.forecasting.arima import ARIMA
 
 from matplotlib import pyplot as plt
 
@@ -13,6 +20,7 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', 1)
 
+warnings.filterwarnings("ignore")
 
 def simple_pseudo_classification():
     """
@@ -62,5 +70,33 @@ def airline_naive_forecast():
     plt.show()
 
 
+def multivariative_forecast(fh):
+    """
+    Multivariative forecaster implemented with VAR (vector auto-regression) algorithm - multivariative
+    (depended on values of other variables)
+    And ARIMA forecaster - univariative (not depended on other variables.)
+    """
+
+    _, y = load_longley()
+    y = y[['POP', 'UNEMP']]
+
+    fc = VAR()
+    fc.fit(y, fh=range(1, fh + 1))
+
+    y_pred = fc.predict()
+    print('VAR model')
+    print(y_pred)
+
+    fc = ARIMA()
+    fc.fit(y, fh=range(1, fh + 1))
+
+    y_pred = fc.predict()
+    print('ARIMA model')
+    print(y_pred)
+
+
 # simple_pseudo_classification()
-airline_naive_forecast()
+# airline_naive_forecast()
+
+
+multivariative_forecast(5)
