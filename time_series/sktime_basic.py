6b1d4a37e99715e5983b448222257c01e66f525d
Alex <oleksandr.molchanov@gmail.com>
2024-02-03 13:07:35 +0200

    Sktime Probabilistic forecast

--- time_series/sktime_basic.py
+++ time_series/sktime_basic.py
@@ -11,6 +11,7 @@ from sktime.utils.plotting import plot_series
 from sktime.forecasting.naive import NaiveForecaster
 from sktime.forecasting.var import VAR
 from sktime.forecasting.arima import ARIMA
+from sktime.forecasting.theta import ThetaForecaster
 
 from matplotlib import pyplot as plt
 
@@ -21,6 +22,7 @@ pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', 1)
 
 warnings.filterwarnings("ignore")
+
 
 def simple_pseudo_classification():
     """
@@ -95,8 +97,51 @@ def multivariative_forecast(fh):
     print(y_pred)
 
 
+def propabilistic_forecast(fh):
+    """
+    predict_interval produces symmetric prediction intervals, for example, a coverage of 0.9 returns a
+    “lower” forecast at quantile 0.5 - coverage/2 = 0.05, and an
+    “upper” forecast at quantile 0.5 + coverage/2 = 0.95.
+
+    The prediction variance is the variance of observations. It is a combination of the variance of the
+    response (standard deviation squared) and the variance of the predicted mean
+    (standard error squared).
+
+    Forecast distributions describes the probability of observing possible future values using the
+    fitted model. The point forecast is the mean of this distribution.
+    """
+
+    y = load_airline()
+
+    fh = np.arange(1, fh + 1)
+
+    fc = ThetaForecaster(sp=12)
+    fc.fit(y, fh=fh)
+
+    y_pred = fc.predict()
+    y_pred_ints = fc.predict_interval(coverage=0.5)
+    print('INTERVALS \n', y_pred_ints)
+
+    y_pred_quant = fc.predict_quantiles(alpha=[0.25, 0.5, 0.75])
+    print('QUANTILES \n', y_pred_quant)
+
+    y_pred_var = fc.predict_var()
+    print('VARIANCE \n', y_pred_var)
+
+    y_pred_proba = fc.predict_proba()
+    print('PROBA \n', y_pred_proba)
+
+    plot_series(
+        y,
+        y_pred,
+        labels=["y", "y_pred"],
+        pred_interval=y_pred_ints,
+        title='Prediction intervals'
+    )
+    plt.show()
+
+
 # simple_pseudo_classification()
 # airline_naive_forecast()
-
-
-multivariative_forecast(5)
+# multivariative_forecast(5)
+propabilistic_forecast(6)
