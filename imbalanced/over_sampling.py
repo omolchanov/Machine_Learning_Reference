75cb8618e185e868a8b257c1f76b002ce5c40938
Alex <oleksandr.molchanov@gmail.com>
2024-02-23 11:40:38 +0200

    Imbalanced classes: over_sampling - BorderlineSMOTE, KMeansSMOTE, SVMSMOTE

--- imbalanced/over_sampling.py
+++ imbalanced/over_sampling.py
@@ -3,17 +3,17 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, recall_score, precision_score
-from sklearn.decomposition import PCA
 
-from imblearn.over_sampling import RandomOverSampler, SMOTE, ADASYN, BorderlineSMOTE
+from imblearn.over_sampling import RandomOverSampler, SMOTE, ADASYN, BorderlineSMOTE, KMeansSMOTE, SVMSMOTE
 from collections import Counter
 
 import pandas as pd
 
-
+# Preparing the dataset
 df = pd.read_csv('../assets/street_alert.csv')
 
 days = {
@@ -31,16 +31,39 @@ times_day = {'morning': 0, 'day': 1, 'evening': 2}
 df['weekday'] = df['weekday'].replace(days)
 df['time_day'] = df['time_day'].replace(times_day)
 
-df = df.groupby('area').filter(lambda x: len(x) >= 4)
+areas = df.groupby(['area']).size().sort_values(ascending=False).to_dict()
+print('Original dataset:', '\n', areas)
+print('N Classes: %.i' % len(areas.keys()))
+
+# Filtering the dataset by number of appearance of a class
+df = df.groupby('area').filter(lambda x: len(x) >= 16)
+filtered_areas = df.groupby(['area']).size().sort_values(ascending=False).to_dict()
+print('\n', 'Filtered dataset:', '\n', filtered_areas)
+print('N Classes: %.i' % len(filtered_areas.keys()))
+
 
 X = df[['weekday', 'time_day']]
 y = df['area']
 
-pca = PCA(n_components=1)
-X_pca = pca.fit_transform(X)
 
-areas = df.groupby(['area']).size().sort_values(ascending=False).to_dict()
-print('Original dataset:', '\n', areas)
+def evaluate_clf(X_resampled, y_resampled):
+    clfs = [
+        KNeighborsClassifier(n_neighbors=1),
+        RandomForestClassifier(n_estimators=1000),
+        LogisticRegression(max_iter=1000),
+        SVC()
+    ]
+
+    X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2)
+
+    for clf in clfs:
+        print('\n', clf)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+
+        print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
+        print('Precision: %.3f' % precision_score(y_test, y_pred, average='weighted', zero_division=True))
+        print('Recall: %.3f' % recall_score(y_test, y_pred, average='weighted', zero_division=True))
 
 
 def naive_random_over_sampling():
@@ -50,30 +73,10 @@ def naive_random_over_sampling():
     print('\n', res)
     print(sorted(Counter(y_resampled).items()))
 
-    X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2)
-
-    clfs = [
-        KNeighborsClassifier(n_neighbors=1),
-        RandomForestClassifier(n_estimators=500),
-        LogisticRegression(max_iter=1000)
-    ]
-
-    for clf in clfs:
-        print('\n', clf)
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
-
-        print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
-        print('Precision: %.3f' % precision_score(y_test, y_pred, average='weighted', zero_division=True))
-        print('Recall: %.3f' % recall_score(y_test, y_pred, average='weighted', zero_division=True))
+    evaluate_clf(X_resampled, y_resampled)
 
 
 def smote_adasyn_sampling():
-    clfs = [
-        KNeighborsClassifier(n_neighbors=1),
-        RandomForestClassifier(n_estimators=1000),
-        LogisticRegression(max_iter=1000)
-    ]
 
     # Sampling with Synthetic Minority Oversampling Technique (SMOTE) method
     sam = SMOTE(k_neighbors=3)
@@ -82,16 +85,7 @@ def smote_adasyn_sampling():
     a = sorted(Counter(y_resampled).items(), key=lambda x: (x[1], x[0]), reverse=True)
     print('\n', sam, ':', a)
 
-    X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2)
-
-    for clf in clfs:
-        print('\n', clf)
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
-
-        print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
-        print('Precision: %.3f' % precision_score(y_test, y_pred, average='weighted', zero_division=True))
-        print('Recall: %.3f' % recall_score(y_test, y_pred, average='weighted', zero_division=True))
+    evaluate_clf(X_resampled, y_resampled)
 
     # Sampling with Adaptive Synthetic (ADASYN) method
     sam = ADASYN(n_neighbors=2, sampling_strategy='minority')
@@ -100,17 +94,39 @@ def smote_adasyn_sampling():
     a = sorted(Counter(y_resampled).items(), key=lambda x: (x[1], x[0]), reverse=True)
     print('\n', sam, ':', a)
 
-    X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2)
+    evaluate_clf(X_resampled, y_resampled)
 
-    for clf in clfs:
-        print('\n', clf)
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
 
-        print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
-        print('Precision: %.3f' % precision_score(y_test, y_pred, average='weighted', zero_division=True))
-        print('Recall: %.3f' % recall_score(y_test, y_pred, average='weighted', zero_division=True))
+def sample_smote_variations():
+
+    # Sampling with Borderline SMOTE
+    sam = BorderlineSMOTE()
+    X_resampled, y_resampled = sam.fit_resample(X, y)
+
+    a = sorted(Counter(y_resampled).items(), key=lambda x: (x[1], x[0]), reverse=True)
+    print('\n', sam, ':', a)
+
+    evaluate_clf(X_resampled, y_resampled)
+
+    # Sampling with Kmeans SMOTE
+    sam = KMeansSMOTE()
+    X_resampled, y_resampled = sam.fit_resample(X, y)
+
+    a = sorted(Counter(y_resampled).items(), key=lambda x: (x[1], x[0]), reverse=True)
+    print('\n', sam, ':', a)
+
+    evaluate_clf(X_resampled, y_resampled)
+
+    # Sampling with SVMSMOTE
+    sam = SVMSMOTE()
+    X_resampled, y_resampled = sam.fit_resample(X, y)
+
+    a = sorted(Counter(y_resampled).items(), key=lambda x: (x[1], x[0]), reverse=True)
+    print('\n', sam, ':', a)
+
+    evaluate_clf(X_resampled, y_resampled)
 
 
 naive_random_over_sampling()
 smote_adasyn_sampling()
+sample_smote_variations()
