9e4f8350a5bceefc2445b2a06df555718b1151af
Alex <oleksandr.molchanov@gmail.com>
2024-03-22 17:39:31 +0200

    Data Analysis: Classification preparation - additional pre-processing

--- data_analysis/classification_preparation.py
+++ data_analysis/classification_preparation.py
@@ -9,6 +9,8 @@ from sklearn.model_selection import KFold, cross_validate
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.pipeline import Pipeline
 
+from imblearn.over_sampling import SMOTE
+
 import pandas as pd
 import numpy as np
 
@@ -27,6 +29,10 @@ df = pd.read_csv('../assets/winequality-white.csv', sep=';')
 
 
 def build_whiskey_plot():
+    """
+    Builds a Whiskey plot for easier identification of outliers
+    """
+
     fig, ax = plt.subplots(1, df.shape[1], sharex=False)
 
     for i, c in enumerate(df.columns):
@@ -37,6 +43,12 @@ def build_whiskey_plot():
 
 
 def print_result(func):
+    """
+    Decorator for various pre-processing functions for printing the dataframe's shape
+    :param func: pre-processing function
+    :return: wrapper
+    """
+
     def wrapper(*args):
         func(*args)
         print('\n', func.__name__, df.shape)
@@ -45,6 +57,10 @@ def print_result(func):
 
 @print_result
 def remove_outliers():
+    """
+    Removes the outliers from all the features basing InterQuartile Range
+    """
+
     outlier_columns = df.columns[:-2]
 
     for c in outlier_columns:
@@ -65,6 +81,10 @@ def remove_outliers():
 
 @print_result
 def remove_unimportant_features():
+    """
+    Removes the unimportant features basing on Descision Tree calculation of importances
+    """
+
     X = df.iloc[:, :-1]
     y = df.iloc[:, -1]
 
@@ -73,20 +93,55 @@ def remove_unimportant_features():
 
     importances = clf.feature_importances_.argsort()[::-1]
 
+    # for i in importances:
+    #     print(df.columns[i])
+
     df.drop(df.columns[importances[-4:]], axis=1, inplace=True)
 
 
 @print_result
-def drop_minority_classes(threshold):
+def drop_minority_classes(threshold=0):
+    """
+    Drops the minority classes basing on the threshold of the class' instances
+    :param threshold: int, default: 0
+    """
+
     idx = df.groupby('quality').filter(lambda x: len(x) < threshold).index
     df.drop(idx, inplace=True)
 
     df.reset_index(drop=True, inplace=True)
 
-    print('\n The distribution of the classes: ', df['quality'].value_counts())
+    # print('\n The distribution of the classes: ', df['quality'].value_counts())
+
+
+@print_result
+def remove_low_correlated_features():
+    """
+    Removes the unimportant features basing on correlation with the target variable
+    """
+
+    corr = df.corr()['quality'].sort_values(ascending=False)
+    print(corr)
+
+    df.drop([
+        'citric acid',
+        'residual sugar',
+        'fixed acidity',
+        'total sulfur dioxide',
+        'volatile acidity',
+        'chlorides',
+        'density'
+    ], axis=1, inplace=True)
 
 
 def preprocess_dataframe():
+    """
+    Preprocess the independent variable with different scalers
+    :return:
+        X_pr - a matrix with pre-processed independent variable
+        y - vector with dependent variable
+    """
+
     X = df.iloc[:, :-1]
     y = df.iloc[:, -1].values
 
@@ -100,36 +155,48 @@ def preprocess_dataframe():
     return X_pr, y
 
 
-drop_minority_classes(500)
-remove_unimportant_features()
+if __name__ == '__main__':
 
-remove_outliers()
-build_whiskey_plot()
+    drop_minority_classes(500)
+    remove_unimportant_features()
+    # remove_low_correlated_features()
 
-print('\n', df.describe())
+    remove_outliers()
+    build_whiskey_plot()
 
-X, y = preprocess_dataframe()
+    print('\n', df.describe())
 
+    X, y = preprocess_dataframe()
 
-def cross_validation(model):
-    k = 5
-    cv = KFold(n_splits=k)
-
-    scores = cross_validate(model, X, y, cv=cv, scoring=['accuracy', 'neg_mean_absolute_error'])
-    return scores
+    # Oversampling
+    X, y = SMOTE().fit_resample(X, y)
 
 
-models = (
-    LogisticRegression(max_iter=10000),
-    # SVC(C=10),
-)
+    def cross_validation(model):
+        """
+        Cross-validates the model
+        :param model
+        :return: scores
+        """
 
-for m in models:
-    print('\n', m)
-    print(df.shape)
+        k = 5
+        cv = KFold(n_splits=k)
 
-    mean_acc = np.mean(cross_validation(m)['test_accuracy'])
-    mean_loss = np.mean(cross_validation(m)['test_neg_mean_absolute_error'])
+        scores = cross_validate(model, X, y, cv=cv, scoring=['accuracy', 'neg_mean_absolute_error'])
+        return scores
 
-    print('Mean accuracy: %.3f' % mean_acc)
-    print('Mean loss: %.3f' % np.abs(mean_loss))
+
+    models = (
+        LogisticRegression(max_iter=10000),
+        SVC(C=10),
+    )
+
+    for m in models:
+        print('\n', m)
+        print(X.shape)
+
+        mean_acc = np.mean(cross_validation(m)['test_accuracy'])
+        mean_loss = np.mean(cross_validation(m)['test_neg_mean_absolute_error'])
+
+        print('Mean accuracy: %.3f' % mean_acc)
+        print('Mean loss: %.3f' % np.abs(mean_loss))
