921ba812f1487ff852e44b92cdfa868d25716c97
Alex <oleksandr.molchanov@gmail.com>
2025-03-10 16:37:47 +0200

    Spark - Configuring remote session and workers

---
+++ spark_examples/session.py
@@ -0,0 +1,19 @@
+from datetime import datetime, date
+
+from pyspark.sql import SparkSession, Row
+
+spark = SparkSession.builder.appName('RemoteTest').remote("sc://localhost:15002").getOrCreate()
+
+
+df = spark.createDataFrame([
+    Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
+    Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
+    Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0))
+])
+# print(df)
+print(df.head())
+
+
+
+
+spark.stop()
