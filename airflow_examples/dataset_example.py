9ce1731f740dbc29d0d4862cc593380a79ab8f4d
Alex <oleksandr.molchanov@gmail.com>
2025-04-19 19:27:28 +0300

    Airflow - Data science pipeline with TPOT - Reworked with Task API

--- airflow_examples/dataset_example.py
+++ airflow_examples/dataset_example.py
@@ -1,22 +1,17 @@
-import io
-import requests
-
 from datetime import datetime
 
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
 from airflow.datasets import Dataset
 
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import RepeatedStratifiedKFold, train_test_split
-from sklearn.metrics import get_scorer
+from sklearn.metrics import classification_report
 
 from tpot import TPOTClassifier
 
 import pandas as pd
-import numpy as np
 
 
 # Configuration
@@ -27,107 +22,86 @@ pd.set_option('display.max_colwidth', 1)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
 
-csv_dataset = Dataset(
-    'https://upnow-prod.ff45e40d1a1c8f7e7de4e976d0c9e555.r2.cloudflarestorage.com/QVVjE28OTFfPalQb6D5tOAXjWiy2/a78cb711'
-    '-2e80-4cf4-af91-4b0e5ed16705?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=cdd12e35bbd220303957dc5603a4cc8e%2F'
-    '20250417%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20250417T212625Z&X-Amz-Expires=43200&X-Amz-Signature=e23e3fa362ec89'
-    '23ca395a9d629b2a5a168df2e2c1e336d1507f827b9f8e2657&X-Amz-SignedHeaders=host&response-content-disposition='
-    'attachment%3B%20filename%3D%22churn-rate.csv%22')
+CSV_DATASET_URI = '/opt/airflow/dags/df/churn-rate.csv'
 
 
-def download_df(**kwargs):
-    try:
-        url = csv_dataset.uri
-        s = requests.get(url).content
-        df = pd.read_csv(io.StringIO(s.decode('utf-8')))
-
-        kwargs['ti'].xcom_push(value=df, key='df')
-
-    except FileNotFoundError:
-        print('Can not download CSV dataframe')
-
-
-def clear_df(**kwargs):
-    df:pd.DataFrame = kwargs['ti'].xcom_pull(task_ids='df_download_task', key='df')
-    df = df.drop(['area_code', 'phone_number'], axis=1)
-
-    print(df.head())
-
-    kwargs['ti'].xcom_push(value=df, key='df')
-
-
-def encode_df(**kwargs):
-    df: pd.DataFrame = kwargs['ti'].xcom_pull(task_ids='clear_df_task', key='df')
-
-    cat_columns = df[['state', 'international_plan', 'voice_mail_plan', 'churn']].columns
-    cat_pipeline = Pipeline(steps=[
-        ('enc', OrdinalEncoder())
-    ])
-
-    ct = ColumnTransformer([('cat_pipeline', cat_pipeline, cat_columns)])
-    df[cat_columns] = ct.fit_transform(df)
-
-    df[cat_columns] = df[cat_columns].astype('int16')
-
-    print(df.info())
-    print(df.head())
-
-    kwargs['ti'].xcom_push(value=df, key='df')
-
-
-def predict_churn(**kwargs):
-    df: pd.DataFrame = kwargs['ti'].xcom_pull(task_ids='encode_df_task', key='df')
-    print(df.head())
-
-    X = df.iloc[:, :-1]
-    y = df['churn']
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-    cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=1, random_state=1)
-
-    model = TPOTClassifier(
-        generations=1,
-        population_size=10,
-        cv=cv,
-        scoring='accuracy',
-        verbosity=2,
-        random_state=1
-    )
-
-    model.fit(X_train, y_train)
-    model.export('clf_tpot_model.py')
-
-    print('Accuracy: %.3f' % (get_scorer('accuracy')(model, X_test, y_test)))
-
-
-with DAG(
+@dag(
     dag_id='dataset_example',
     start_date=datetime(2025, 4, 15),
     schedule='@once',
-    fail_stop=True
+    fail_stop=True,
 
-) as dag:
+)
+def dataset_example_flow():
 
-    download_df_task = PythonOperator(
-        task_id='df_download_task',
-        outlets=csv_dataset,
-        python_callable=download_df,
+    @task(
+        outlets=Dataset(CSV_DATASET_URI),
+        show_return_value_in_logs=False
     )
+    def download_df():
+        try:
+            df = pd.read_csv(CSV_DATASET_URI)
+            print(df.shape)
+            return df
 
-    clear_df_task = PythonOperator(
-        task_id='clear_df_task',
-        python_callable=clear_df
-    )
+        except FileNotFoundError:
+            print('Can not download CSV dataframe')
+            exit(1)
 
-    encode_df_task = PythonOperator(
-        task_id='encode_df_task',
-        python_callable=encode_df
-    )
+    @task(show_return_value_in_logs=False)
+    def clear_df(df):
+        df = df.drop(['area_code', 'phone_number'], axis=1)
+        print(df.head())
 
-    predict_churn_task = PythonOperator(
-        task_id='predict_churn_task',
-        python_callable=predict_churn
-    )
+        return df
 
-    download_df_task >> clear_df_task >> encode_df_task >> predict_churn_task
+    @task(show_return_value_in_logs=False)
+    def encode_df(df):
+        cat_columns = df[['state', 'international_plan', 'voice_mail_plan', 'churn']].columns
+        cat_pipeline = Pipeline(steps=[
+            ('enc', OrdinalEncoder())
+        ])
+
+        ct = ColumnTransformer([('cat_pipeline', cat_pipeline, cat_columns)])
+        df[cat_columns] = ct.fit_transform(df)
+
+        df[cat_columns] = df[cat_columns].astype('int16')
+
+        print(df.info())
+        # print(df.head())
+
+        return df
+
+    @task()
+    def predict_churn(df):
+        print(df.shape)
+
+        X = df.iloc[:, :-1]
+        y = df['churn']
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+        cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=1, random_state=1)
+
+        model = TPOTClassifier(
+            generations=1,
+            population_size=10,
+            cv=cv,
+            scoring='accuracy',
+            verbosity=2,
+            random_state=1
+        )
+
+        model.fit(X_train, y_train)
+        model.export('clf_tpot_model.py')
+
+        y_pred = model.fitted_pipeline_.predict(X_test)
+        print(classification_report(y_pred, y_test))
+
+    df = download_df()
+    df_cleared = clear_df(df)
+    df_encoded = encode_df(df_cleared)
+    predict_churn(df_encoded)
+
+
+dataset_example_flow()
