21ed7c627ab9a55597cc390a71a47762b532d2ba
Alex <oleksandr.molchanov@gmail.com>
2025-04-20 12:35:38 +0300

    Airflow - Conditional flow and parallel flows

---
+++ airflow_examples/flow_example.py
@@ -0,0 +1,89 @@
+# https://stackoverflow.com/questions/43678408/how-to-create-a-conditional-task-in-airflow
+
+from datetime import datetime
+
+from airflow.decorators import dag, task, task_group, branch_task
+from airflow.datasets import Dataset
+
+
+@dag(
+    dag_id='flow_example',
+    start_date=datetime(2025, 4, 15),
+    catchup=False
+)
+def dataset_flow_example():
+
+    @task_group(group_id='parallel_tasks')
+    def run_parallel_tasks():
+
+        @task(task_id='task1')
+        def task_1():
+            print('I am task 1')
+
+        @task(task_id='task3')
+        def task_3():
+            print('I am task 3')
+
+        task_1()
+        task_3()
+
+    @task_group(group_id='dependent_tasks')
+    def run_dependent_tasks():
+        @task
+        def task_2():
+            print('I am task 2')
+
+        @task
+        def task_4():
+            print('I am task 4')
+
+        task_2() >> task_4()
+
+    run_parallel_tasks() >> run_dependent_tasks()
+
+
+dataset_flow_example()
+
+
+@dag(
+    dag_id='conditional_flow',
+    start_date=datetime(2025, 4, 15),
+    catchup=False
+)
+def conditional_flow_example():
+
+    @task(task_id='task1')
+    def task_1(**kwargs):
+        print('task1')
+        kwargs['ti'].xcom_push(key='number', value=15)
+
+    @branch_task(task_id='branch_task_2')
+    def task_2(**kwargs):
+        print('task2')
+
+        n = kwargs['ti'].xcom_pull(task_ids='task1', key='number')
+        print(n)
+
+        if n > 10:
+            return 'task3'
+
+        elif n < 10:
+            return 'task4'
+
+        else:
+            return None
+
+    @task(task_id='task3')
+    def task_3():
+        print('task3')
+
+    @task(task_id='task4')
+    def task_4():
+        print('task4')
+
+    task_1()
+    task_2() >> task_3()
+    task_2() >> task_4()
+
+
+conditional_flow_example()
