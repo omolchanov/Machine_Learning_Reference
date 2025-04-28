e2e05aa10820fc8e7c982273513f35321d8cbdcf
Alex <oleksandr.molchanov@gmail.com>
2025-04-28 14:52:38 +0300

    ETL - (Bronze) Load raw Anomalies_Analysis data  to MySQL - Local

--- projects/churn-rate-etl/churn_rate_local.py
+++ projects/churn-rate-etl/churn_rate_local.py
@@ -1,5 +1,7 @@
 import csv
+
 import mysql.connector
+from pymongo import MongoClient
 
 
 # Paths and connections
@@ -21,6 +23,10 @@ bronze_db = mysql.connector.connect(
     database='churn_rate_bronze'
 )
 
+mongo_db_client = MongoClient(
+    'mongodb+srv://oleksandrmolchanov:test123@churn-rate-scam.bnuspoh.mongodb.net/'
+    '?retryWrites=true&w=majority&appName=churn-rate-scam')
+
 
 def load_csv_dataset_to_bronze_db():
     bronze_db_cursor = bronze_db.cursor()
@@ -39,7 +45,6 @@ def load_csv_dataset_to_bronze_db():
         rows = [row for row in reader]
 
         sql = f"INSERT INTO {table} ({', '.join(headers)}) VALUES ({', '.join(['%s'] * len(headers))})"
-
         bronze_db_cursor.executemany(sql, rows)
         bronze_db.commit()
 
@@ -85,6 +90,51 @@ def load_mysql_raw_dataset_to_bronze_db():
     # raw_db.close()
 
 
+def load_mongodb_anomalies_analysis_to_bronze_db():
+
+    # Get the scam data from MongoDB
+    mongo_db = mongo_db_client.churn_rate
+    mongo_collection = mongo_db.anomalies_analysis
+
+    rows = []
+    mongo_cursor = mongo_collection.find({})
+    for d in mongo_cursor:
+        d['_id'] = str(d['_id'])
+        d.update()
+
+        r = tuple(d.values())
+        rows.append(r)
+
+    table = 'Anomalies_Analysis'
+
+    headers = [
+        'mongo_id',
+        'customer_id',
+        'phone_number',
+        'outgoing_phone_number',
+        'timestamp',
+        'fraud_scoring',
+        'is_fraud'
+    ]
+
+    bronze_db_cursor = bronze_db.cursor()
+
+    # Truncate the Bronze DB table
+    sql = f"TRUNCATE TABLE {table}"
+    bronze_db_cursor.execute(sql)
+
+    insert_sql = f"INSERT INTO {table} ({', '.join(headers)}) VALUES ({', '.join(['%s'] * len(headers))})"
+    bronze_db_cursor.executemany(insert_sql, rows)
+    bronze_db.commit()
+
+    # bronze_db_cursor.close()
+    # bronze_db.close()
+
+    mongo_cursor.close()
+
+
 load_csv_dataset_to_bronze_db()
 load_mysql_raw_dataset_to_bronze_db()
+load_mongodb_anomalies_analysis_to_bronze_db()
+
 
